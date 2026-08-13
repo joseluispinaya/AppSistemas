973800664cadcfe1630ac766554614c992ce1a92
zerobyte <joseluisdelta1@gmail.com>
2026-08-13 15:56:36 -0400

    moejora entrega

--- navigation/Navigation.js
+++ navigation/Navigation.js
@@ -5,14 +5,50 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import InicioStack from './InicioStack';
 import HistoriaStack from './HistoriaStack'
 import AccountStack from './AccountStack'
+import { Ionicons } from '@expo/vector-icons';
+import Toast from 'react-native-toast-message';
 
 const Tab = createBottomTabNavigator();
 
 export default function Navigation() {
 
+    const screenOptions = (route, color) => {
+        let iconName;
+
+        switch (route.name) {
+            case "home":
+                iconName = "home";
+                break;
+            case "historia":
+                iconName = "newspaper";
+                break;
+            case "account":
+                iconName = "person";
+                break;
+            default:
+                iconName = "help-circle";
+                break;
+        }
+        return (
+            <Ionicons
+                name={iconName}
+                size={24}
+                color={color}
+            />
+        );
+    };
+
     return (
         <NavigationContainer>
-            <Tab.Navigator>
+            <Tab.Navigator
+                initialRouteName="home"
+                screenOptions={({ route }) => ({
+                    tabBarIcon: ({ color }) => screenOptions(route, color),
+                    tabBarActiveTintColor: "#442484",
+                    tabBarInactiveTintColor: "#a17dc3",
+                    headerShown: false
+                })}
+            >
                 <Tab.Screen name="home" component={InicioStack} options={{ title: "Inicio" }} />
                 <Tab.Screen name="historia" component={HistoriaStack} options={{ title: "Historia" }} />
                 <Tab.Screen name="account" component={AccountStack} options={{ title: "Cuenta" }} />
